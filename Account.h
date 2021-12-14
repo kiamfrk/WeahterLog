b8a772322cce3f6f1b78cd12d2eb2fc004c5b6d5
Frank Kiam <57965304+kiamfrk@users.noreply.github.com>
2021-12-14 10:07:26 +0100

    Account.h

--- Account.h
+++ Account.h
@@ -6,15 +6,15 @@ alongside your sketch. To do so, make a new tab in Arduino, call it TembooAccoun
 and copy this content into it. 
 */
 
-#define TEMBOO_ACCOUNT "uriche"  // Your Temboo account name 
+#define TEMBOO_ACCOUNT "yourTembooName"  // Your Temboo account name 
 #define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
-#define TEMBOO_APP_KEY "e7UdSLWc3nSmrj6eOVUbf9IM20LiUCOz"  // Your Temboo app key
+#define TEMBOO_APP_KEY "yourTembooKey"  // Your Temboo app keyapp key
 
 
-#define GOOGLE_CLIENT_ID "45712978547-fj9e1nl7ae84e86d8srmssfq75kun73g.apps.googleusercontent.com"
-#define GOOGLE_CLIENT_SECRET "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"
-#define GOOGLE_CLIENT_REFRESH_TOKEN "1//0dJq2DRWtDN13CgYIARAAGA0SNwF-L9IrHgUGdrER94kkGrSiVxKr8euUdmA7mzxRydnVjXc4F14RSKsH1yELSDkWS-yJOoRVmsk"
-#define GOOGLE_SPREADSHEET_ID "1YIHLM_pbOZqU4_DD50s-SlE_1Q6b7FCfbTnPb_bGQE4" 
+#define GOOGLE_CLIENT_ID "Google-client-id"
+#define GOOGLE_CLIENT_SECRET "Your-googleclient-secret-code"
+#define GOOGLE_CLIENT_REFRESH_TOKEN "The refresh-token"
+#define GOOGLE_SPREADSHEET_ID "Your spreadsheetsID" 
 
 #if TEMBOO_LIBRARY_VERSION < 2
 #error "Your Temboo library is not up to date. You can update it using the Arduino library manager under Sketch > Include Library > Manage Libraries..."
